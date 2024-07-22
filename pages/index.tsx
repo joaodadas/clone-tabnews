32b970f78a97519a2f77f1ebd52b4c7714d6b6b8
João Vitor Dadas <93526863+joaodadas@users.noreply.github.com>
2024-07-22 15:34:59 +0000

    adiciona arquivo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import react from 'react'
 
 function Home() {
-    return <h1 style={{display: "flex", justifyContent: "center", color: "babyblue"}}>Tab News</h1>
+  return <h1 style={{display: "flex", justifyContent: "center", color: "babyblue"}}>Tab News</h1>
 }
 
-export default Home
+export default Home   
