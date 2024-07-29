55c93ee8772917395e61585f8f0484aa82dbabb5
Joao Vitor <joao@cargon.com.br>
2024-07-29 15:59:43 -0300

    add `database.ts` and `.env` files

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,4 +1,8 @@
-function status(requst: any, response: any) {
+import database from "../../../../infra/database";
+
+async function status(requst: any, response: any) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "valor" });
 }
 
