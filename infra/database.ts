309565f6f9f4a1dd2d1f5127dbe2b123666fa151
Joao Vitor <joao@cargon.com.br>
2024-08-07 09:35:07 -0300

    fix data base implementations to catch error

--- infra/database.ts
+++ infra/database.ts
@@ -9,13 +9,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("credenciais postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
