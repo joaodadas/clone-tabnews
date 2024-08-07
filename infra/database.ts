bdc1affdd72e204c1ec71c8a32fc87dd7faa2e1a
Joao Vitor <joao@cargon.com.br>
2024-08-07 11:41:33 -0300

    handles database ssl conection

--- infra/database.ts
+++ infra/database.ts
@@ -7,14 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-
-  console.log("credenciais postgres: ", {
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   try {
