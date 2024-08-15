35026ab55880612131c78d9b738ae8877ac0e4a9
Joao Vitor <joao@cargon.com.br>
2024-08-15 16:05:39 -0300

    adds `/ap1/v1/migrations` endpoint

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -1,29 +1,35 @@
-import migrationRunner from "node-pg-migrate";
+import migrationRunner, { RunnerOption } from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(requst: any, response: any) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions: RunnerOption = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (requst.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    return response.status(200).json(migrations);
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   }
 
   if (requst.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
-    return response.status(200).json(migrations);
+    await dbClient.end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405).end();
